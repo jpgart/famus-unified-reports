fa86342e4dc27417e1794e400acf6a541c9489f9
Juan Pablo Garcia Abalos <jpgart@gmail.com>
2025-06-20 11:54:18 -0400

    Add GitHub Pages deployment configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
+    publicPath: '/famus-unified-reports/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
